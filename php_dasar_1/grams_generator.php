2bb5d05321277ca32e88e37ad62698acbaf4e31f
waras47 <saudinwahyudin@gmail.com>
2024-12-14 04:31:43 +0700

    change file gram generator

--- php_dasar_1/grams_generator.php
+++ php_dasar_1/grams_generator.php
@@ -2,12 +2,13 @@
 
 function generateGrams($input)
 {
+ // Normalisasi input: kecilkan semua huruf dan hilangkan karakter non-alfabet
  $input = strtolower(trim($input));
  $input = preg_replace('/[^\w\s]/', '', $input);
 
+ // Pisahkan input menjadi array 
  $words = explode(' ', $input);
-
-
+ 
  $unigrams = [];
  $bigrams = [];
  $trigrams = [];
@@ -16,13 +17,17 @@ function generateGrams($input)
  $unigrams = $words;
 
  // Generate Bigram
- for ($i = 0; $i < count($words) - 1; $i++) {
-  $bigrams[] = $words[$i] . ' ' . $words[$i + 1];
+ for ($i = 0; $i < count($words); $i += 2) { 
+  if ($i + 1 < count($words)) {
+   $bigrams[] = $words[$i] . ' ' . $words[$i + 1];
+  }
  }
 
  // Generate Trigram
- for ($i = 0; $i < count($words) - 2; $i++) {
-  $trigrams[] = $words[$i] . ' ' . $words[$i + 1] . ' ' . $words[$i + 2];
+ for ($i = 0; $i < count($words); $i += 3) { 
+  if ($i + 2 < count($words)) {
+   $trigrams[] = $words[$i] . ' ' . $words[$i + 1] . ' ' . $words[$i + 2];
+  }
  }
 
  // Format output
@@ -35,7 +40,6 @@ function generateGrams($input)
  return $output;
 }
 
-
 $input = "Jakarta adalah ibukota negara Republik Indonesia";
 $output = generateGrams($input);
 
@@ -43,5 +47,3 @@ $output = generateGrams($input);
 echo "Unigram : " . $output['Unigram'] . "\n";
 echo "Bigram : " . $output['Bigram'] . "\n";
 echo "Trigram : " . $output['Trigram'] . "\n";
-
-?>
